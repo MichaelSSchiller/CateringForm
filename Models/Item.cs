f1166b998581645832540a282f6c7e5588375a59
MichaelSSchiller <38842472+MichaelSSchiller@users.noreply.github.com>
2018-08-29 11:57:17 -0500

    Add files via upload

---
+++ Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SRHCafe.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public IEnumerable<Item> SubOptions { get; set; }
+        public int NumberOfPeople { get; set; }
+        public int NumberOfEntrees { get; set; }
+        public int QTY { get; set; }
+    }
+}
